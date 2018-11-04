b10a10e6cf6e6a9a62334e301e65f9bfd56d1b26
Richard Heaton <richard.heaton@coxautoinc.com>
2018-11-04 08:58:36 -0500

    change to accomadate multiple sessions

---
+++ darkStar/11_4_18/app.js
@@ -0,0 +1,25 @@
+var array = [{
+  id: 0,
+  name: 'richard',
+  email: 'richard.k.heaton@gmail.com'
+},{
+  id: 1,
+  name: 'ben',
+  email: 'ben@gmail.com'
+},{
+  id: 2,
+  name: 'samip',
+  email: 'samip@gmail.com'
+},{
+  id: 3,
+  name: 'shelby',
+  email: 'shelby@gmail.com'
+},{
+  id: 4,
+  name: 'heaven',
+  email: 'heaven@gmail.com'
+},{
+  id: 5,
+  name: 'levi',
+  email: 'levi@gmail.com'
+}];
